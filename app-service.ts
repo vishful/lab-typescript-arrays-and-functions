3db9838a5c18512b3b74f7452e326c43967a7e2f
FACEPrep-ProGrad <61002120+FACEPrep-ProGrad@users.noreply.github.com>
2020-06-08 18:40:38 +0530

    Update app-service.ts

--- app-service.ts
+++ app-service.ts
@@ -1,7 +1,3 @@
 export interface ArrayService {
-  arrayMultiply(myArray : any) : Array<Number>;
-  arraySeparate(myArray : any) : Array<string>;
-  arraySplit(myString : string) : Array<number>;
-  arraySort(myArray : any) : Array<string>;
-  arrayReplace(myArray : any) : Array<Number>;
+
 }
