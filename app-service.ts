43e8ec7e8731a95d35cd7c912a0daf8c38e8d44a
Vishnu <vishnurpillai9914@gmail.com>
2020-07-31 03:01:00 +0530

    FSJ0520A101

--- app-service.ts
+++ app-service.ts
@@ -1,3 +1,7 @@
 export interface ArrayService {
-
+    arrayFindMultiples(myArray : any) : Array<any>;
+    arraySeparate(myArray : any) : Array<any>;
+    arraySplit(myString : string) : Array<String>;
+    arraySort(myArray : any) : Array<any>;
+    arrayReplace(myArray : any) : Array<any>;
 }
--- main.ts
+++ main.ts
@@ -5,23 +5,60 @@ export class ArrayComponent implements ArrayService {
     constructor() {}
 
     public arrayMultiply(myArray: number[]) : Array<Number> {
+        var multiples;
+
+        for (var i = 0; i < myArray.length; i++) {
+          if (myArray[i] % 5 == 0) {
+            multiples : myArray[i];
+          }
+          else if (myArray[i] % 10 == 0) {
+            multiples : myArray[i];
+          }
+        }
       
+        return multiples;
     }
 
-    public arraySeparate(myArray: any) : Array<string>{
-        
+    public arraySeparate(myArray: any) : Array<String> {
+        var count:number=0;
+        for(var i=0;i<myArray.length;i++){
+            if(typeof myArray[i]=="string"){
+                count++;
+                return myArray[i];
+            }
+        }
     }
 
-    public arraySplit(str:string) : Array<number>{
+    /*public arraySplit(str:string) : Array<number>{
+        /*var count:number=0;
+        for(var i=0;i<myArray.length;i++){
+            if(typeof myArray[i]=="number"){
+                count++;
+                return myArray[i];
+          
+    }*/
 
-    }
-
-    public arraySort(myArray:any):Array<string>{
-      
+    public arraySort(myArray:any) : Array<string>{
+        var size=myArray.length;
+        for(var i = 0; i<size-1; i++) {
+            for (var j = i+1; j<myArray.length; j++) {
+               if(myArray[i].compareTo(myArray[j])>0) {
+                  var temp = myArray[i];
+                  myArray[i] = myArray[j];
+                  myArray[j] = temp;
+               }
+            }
+           return myArray;
     }
 
     public arrayReplace(myArray:any):Array<number>{
-       
+        var multiples;
+
+        for (var i = 0; i < myArray.length; i++) {
+          if (myArray[i] % 3 == 0) {
+            myArray[i]=5;
+          }
+          return myArray;
 }
 
 let myArray: number[] = [34, 45, 60, 23, 13, 25, 70];
@@ -32,5 +69,6 @@ console.log(array.arraySeparate(myArray));
 console.log(array.arraySort(myArray));
 console.log(array.arrayReplace(myArray));
 
+    }
 
-
+    }
